08a0082d4532944505f764c09f9f25be985fa645
Jonathan Lucerio <jonathanlucerio@gmail.com>
2024-02-01 16:27:47 +0800

    adjust responsiveness

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import Laptop from '../assets/Laptop1.png'
 
 const Hero = () => {
   return (
-    <div className='text-white md:flex space-x-10 mx-8'>
+    <div className='text-white md:flex md:space-x-10 mx-8'>
         <div className='w-[600px] md:max-w-[800px] md:mt-[-96px] h-[500px] mt-10 md:h-screen flex flex-col justify-center gap-0 sm:ml-auto md:ml-40 ml-auto'>
             <div className='flex text-2xl md:text-3xl font-bold space-x-2'>
               <h1 className='font-bold'>An Intellectual </h1><h1 className='text-[#ff8730]'>Imagination</h1>
@@ -20,7 +20,7 @@ const Hero = () => {
             <button className='bg-[#ff8730] w-[200px] rounded-full font-bold my-6 py-3 text-black'>Shop Now</button>
         </div>
         <div>
-          <img src={Laptop} alt="/" className='w-[900px] md:mt-[120px] justify-center'></img>
+          <img src={Laptop} alt="/" className='w-[900px] md:mt-[120px] justify-center mb-24 md:mb-0'></img>
         </div>
     </div>
   )
