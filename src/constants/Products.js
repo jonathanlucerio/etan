8e8aa7ae385b1c27d50c5a546cabe0758c46f709
Jonathan Lucerio <jonathanlucerio@gmail.com>
2024-02-02 20:49:56 +0800

    add swiper in products

---
+++ src/constants/Products.js
@@ -0,0 +1,52 @@
+import {
+    RxCrop,
+    RxDesktop,
+    RxPencil2,
+    RxReader,
+    RxRocket,
+    RxAccessibility,
+    RxLaptop,
+    RxTimer,
+    RxSpeakerLoud,
+    RxPencil1,
+    RxHeart,
+  } from "react-icons/rx";
+  
+  import Product1 from "../assets/Product1.jpg";
+  import Product2 from "../assets/Product2.jpg";
+  import Product3 from "../assets/Product3.jpg";
+  import Product4 from "../assets/Product4.jpg";
+  import Product5 from "../assets/Product5.jpg";
+  
+  export const ServiceData = [
+    {
+      icon: RxLaptop,
+      title: "Laptop",
+      content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+      backgroundImage: Product1,
+    },
+    {
+      icon: RxHeart,
+      title: "Phone",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: Product2,
+    },
+    {
+      icon: RxPencil2,
+      title: "Tablet",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: Product3,
+    },
+    {
+      icon: RxTimer,
+      title: "Smart Watch",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: Product4,
+    },
+    {
+      icon: RxSpeakerLoud,
+      title: "Airpods",
+      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      backgroundImage: Product5,
+    },
+  ];
