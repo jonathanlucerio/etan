68e7c6e74d7a41280f036267c63a0365b80f7279
Jonathan Lucerio <jonathanlucerio@gmail.com>
2024-02-03 22:48:05 +0800

    Fix Swiper Navigation

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -3,8 +3,9 @@ import {Swiper, SwiperSlide} from 'swiper/react'
 import 'swiper/css'
 import'swiper/css/pagination'
 import 'swiper/css/free-mode'
+import 'swiper/css/navigation'
 
-import {FreeMode, Pagination} from 'swiper/modules'
+import {FreeMode, Pagination, Navigation, Mousewheel, Keyboard} from 'swiper/modules'
 import {RxArrowTopRight} from 'react-icons/rx'
 import { ServiceData } from '../constants/Products'
 
@@ -13,26 +14,29 @@ function Products() {
     <div className='flex item-center justify-center flex-col h-screen bg-black'>
       <Swiper
         breakpoints={{
-          340:{
+          500:{
             slidesPerView: 2,
-            spaceBetween: 15,
+            spaceBetween: 10
           },
-          700:{
+          900:{
             slidesPerView: 3,
-            spaceBetween: 15,
+            spaceBetween: 15
           }
         }}
-        
+        cssMode={true}
+        navigation={true}
         freeMode={true}
+        mousewheel={true}
+        keyboard={true}
         pagination ={{
           clickable: true,
         }}
-        modules ={[FreeMode, Pagination]}
-        className='max-w-[90%] lg:max-w-[80%]'
+        modules ={[FreeMode, Pagination, Navigation, Mousewheel, Keyboard]}
+        className='max-w-[90%]'
         >
         {ServiceData.map((item) => (
           <SwiperSlide key={item.title}>
-              <div className='flex flex-col gap-6 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[250px] w-[215px] lg:h-[400px] lg:w-[450px] overflow-hidden cursor-pointer'>
+              <div className='flex flex-col gap-6 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[200px] w-[225px] lg:h-[400px] lg:w-[450px] overflow-hidden cursor-pointer mx-auto'>
                 <div
                 className='absolute inset-0 bg-cover bg-center'
                 style={{ backgroundImage:  `url(${item.backgroundImage})`}}
