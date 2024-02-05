df0e3c8cc54d5e2e0f4469c7159fa46bfc365c49
Jonathan Lucerio <jonathanlucerio@gmail.com>
2024-02-05 18:46:26 +0800

    Add Contents on Product Page

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -4,6 +4,7 @@ import 'swiper/css'
 import'swiper/css/pagination'
 import 'swiper/css/free-mode'
 import 'swiper/css/navigation'
+import LaptopVideo from '../assets/laptopvid1.mp4'
 
 import {FreeMode, Pagination, Navigation} from 'swiper/modules'
 import {RxArrowTopRight} from 'react-icons/rx'
@@ -11,7 +12,8 @@ import { ServiceData } from '../constants/Products'
 
 function Products() {
   return (
-    <div className='flex item-center justify-center flex-col h-screen bg-black'>
+    <div className='flex item-center justify-center flex-col h-auto md:h-screen md:mt-10 bg-black'>
+      <h1 className='font-bold text-[#ff8730] text-4xl text-center my-14'>Featured Products</h1>
       <Swiper
         breakpoints={{
           500:{
@@ -29,11 +31,11 @@ function Products() {
           clickable: true,
         }}
         modules ={[FreeMode, Pagination, Navigation]}
-        className='max-w-[90%]'
+        className='max-w-[85%]'
         >
         {ServiceData.map((item) => (
           <SwiperSlide key={item.title}>
-              <div className='flex flex-col gap-6 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[200px] w-[225px] lg:h-[400px] lg:w-[450px] overflow-hidden cursor-pointer mx-auto'>
+              <div className='flex flex-col gap-6 group relative shadow-lg text-white rounded-xl px-6 py-8 h-[200px] w-[225px] lg:h-[300px] lg:w-[400px] overflow-hidden cursor-pointer mx-auto'>
                 <div
                 className='absolute inset-0 bg-cover bg-center'
                 style={{ backgroundImage:  `url(${item.backgroundImage})`}}
@@ -49,6 +51,18 @@ function Products() {
           </SwiperSlide>
         ))}
       </Swiper>
+      <div className='z-10 md:h-100% md:grid md:grid-cols-2 w-100% my-16 mx-8 md:mx-48 gap-5'>
+        <video className='w-[600px] rounded-lg my-14 md:my-0' src={LaptopVideo} loop autoPlay muted/>
+        <div className='mx-4'>
+          <div className='flex space-x-2 text-3xl font-semibold mb-5 md:mb-4'>
+            <h1 className='text-[#ff8730]'>Elevate</h1><h1 className='text-white'>Your Device</h1>
+          </div>
+          
+          <p className='w-100% md:w-[500px] text-white opacity-70'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae cumque eum quidem a error aut voluptatem sunt, assumenda optio officia voluptates minus ex? Soluta optio nam adipisci magni necessitatibus ipsam!</p>
+          <br/>
+          <p className='w-100% md:w-[500px] text-white opacity-70'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, mollitia atque facere ex amet earum tempore numquam commodi! Rerum eaque iure velit nulla autem nisi hic dignissimos modi tempore qui?</p>
+        </div>
+      </div>
     </div>
   )
 }
